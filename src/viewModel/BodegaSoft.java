b8f8bf491dd5a41b6701395edb6f337209a1a799
diegofmendez <tu@email.com>
2025-02-20 14:05:57 -0500

    commit

---
+++ src/viewModel/BodegaSoft.java
@@ -0,0 +1,16 @@
+
+package viewModel;
+
+import view.*;
+
+public class BodegaSoft {
+
+    public static void main(String[] args) {
+        
+        Usuario jorge = new Usuario("Jorge","contrasena",1);
+        Login login = new Login(jorge);
+        
+        
+    }
+    
+}
